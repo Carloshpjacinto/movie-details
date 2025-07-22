69ef022adbe8040987b1929a30f82d5f7fa60a9d
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-07-22 14:33:32 -0300

    feature/finishing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ProgressProviderWrapper>{children}</ProgressProviderWrapper>
+        <ProgressProviderWrapper> {children} </ProgressProviderWrapper>
       </body>
     </html>
   );
