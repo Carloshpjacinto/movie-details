12b1ee4bcafe6fdc3e6900688bdb2c1036ef64cf
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-07-22 00:22:35 -0300

    feature/responsiveness-nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -24,7 +24,7 @@ const Navbar = ({ searchQuery = "", onSearchChange }: NavbarProps) => {
           height={500}
           quality={90}
           priority
-          className="rounded-lg w-30 h-15 object-cover ml-[2rem]"
+          className="rounded-lg w-30 h-15 object-cover ml-[2rem] max-sm:w-20 max-sm:ml-[1rem]"
         />
       </Link>
       <div className="flex flex-row items-center">
@@ -36,10 +36,10 @@ const Navbar = ({ searchQuery = "", onSearchChange }: NavbarProps) => {
               width={500}
               height={500}
               quality={90}
-              className="rounded-lg w-6 h-6 object-cover mr-[.5rem]"
+              className="rounded-lg w-6 h-6 object-cover mr-[.5rem] max-sm:hidden"
             />
             <input
-              className="border border-black rounded-full text-black px-4 py-2 mr-25"
+              className="border border-black rounded-full text-black px-4 py-2 mr-25 max-sm:mr-2"
               placeholder="Pesquisar..."
               type="text"
               value={searchQuery}
@@ -48,7 +48,7 @@ const Navbar = ({ searchQuery = "", onSearchChange }: NavbarProps) => {
           </>
         ) : (
           <button
-            className="border border-black rounded-full text-black px-4 py-2 mr-25 cursor-pointer"
+            className="border border-black rounded-full text-black px-4 py-2 mr-25 cursor-pointer max-sm:mr-4"
             onClick={() => router.push("/")}
           >
             Voltar
